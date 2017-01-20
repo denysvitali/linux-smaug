e0462ba2906187274f6d5daa8b52bdfcff8a8681
Thierry Reding <treding@nvidia.com>
2017-01-20 14:40:23 +0100

    system-power: Add system power and restart framework
    This adds a very simple framework that allows drivers to register system
    power and restart controllers. The goal of this framework is to replace
    the current notifier based mechanism for restart handlers and the power
    off equivalent that is the global pm_power_off() function.
    
    Both of these approaches currently lack any means of locking against
    concurrently registering handlers or formal definitions on what proper
    priorities are to order handlers.
    
    The system-power framework attempts to remedy this by adding a system
    power chip object that drivers can embed in their driver-specific data.
    A chip contains a description of capabilities that the framework uses
    to determine a good sequence of handlers to use for restart and power
    off.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    ---
    Changes in v2:
    - implement some missing functionality
    - remove extra struct system_power_ops
    - sort chips by level

---
+++ drivers/base/system-power.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2017 NVIDIA Corporation
+ *
+ * This file is released under the GPL v2
+ */
+
+#define pr_fmt(fmt) "system-power: " fmt
+
+#include <linux/system-power.h>
+
+static DEFINE_MUTEX(system_power_lock);
+static LIST_HEAD(system_power_chips);
+
+static const char *spc_get_name(struct system_power_chip *chip)
+{
+	if (chip->name)
+		return chip->name;
+
+	if (chip->dev)
+		return dev_name(chip->dev);
+
+	return "";
+}
+
+#define spc_warn(chip, fmt, ...)				\
+	pr_warn("%s: " fmt, spc_get_name(chip), ##__VA_ARGS__)
+
+#define spc_dbg(chip, fmt, ...)					\
+	pr_debug("%s: " fmt, spc_get_name(chip), ##__VA_ARGS__)
+
+int system_power_chip_add(struct system_power_chip *chip)
+{
+	struct system_power_chip *node;
+
+	if (!chip->restart && !chip->power_off) {
+		WARN(1, pr_fmt("must implement restart or power off\n"));
+		return -EINVAL;
+	}
+
+	INIT_LIST_HEAD(&chip->list);
+
+	mutex_lock(&system_power_lock);
+
+	list_for_each_entry(node, &system_power_chips, list)
+		if (chip->level > node->level)
+			break;
+
+	list_add_tail(&chip->list, &node->list);
+
+	mutex_unlock(&system_power_lock);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(system_power_chip_add);
+
+int system_power_chip_remove(struct system_power_chip *chip)
+{
+	mutex_lock(&system_power_lock);
+
+	list_del_init(&chip->list);
+
+	mutex_unlock(&system_power_lock);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(system_power_chip_remove);
+
+bool system_can_power_off(void)
+{
+	struct system_power_chip *chip;
+
+	mutex_lock(&system_power_lock);
+
+	list_for_each_entry(chip, &system_power_chips, list) {
+		if (chip->power_off) {
+			mutex_unlock(&system_power_lock);
+			return true;
+		}
+	}
+
+	mutex_unlock(&system_power_lock);
+
+	/* XXX for backwards compatibility */
+	return pm_power_off != NULL;
+}
+
+int system_restart(char *cmd)
+{
+	struct system_power_chip *chip;
+	int err;
+
+	mutex_lock(&system_power_lock);
+
+	list_for_each_entry(chip, &system_power_chips, list) {
+		if (!chip->restart_prepare)
+			continue;
+
+		spc_dbg(chip, "preparing to restart...\n");
+
+		err = chip->restart_prepare(chip, reboot_mode, cmd);
+		if (err < 0)
+			spc_warn(chip, "failed to prepare restart: %d\n", err);
+	}
+
+	list_for_each_entry(chip, &system_power_chips, list) {
+		if (!chip->restart)
+			continue;
+
+		spc_dbg(chip, "restarting...\n");
+
+		err = chip->restart(chip, reboot_mode, cmd);
+		if (err < 0)
+			spc_warn(chip, "failed to restart: %d\n", err);
+	}
+
+	mutex_unlock(&system_power_lock);
+
+	/* XXX for backwards compatibility */
+	do_kernel_restart(cmd);
+
+	return 0;
+}
+
+int system_power_off_prepare(void)
+{
+	/* XXX for backwards compatibility */
+	if (pm_power_off_prepare)
+		pm_power_off_prepare();
+
+	return 0;
+}
+
+int system_power_off(void)
+{
+	struct system_power_chip *chip;
+	int err;
+
+	mutex_lock(&system_power_lock);
+
+	list_for_each_entry(chip, &system_power_chips, list) {
+		if (!chip->power_off_prepare)
+			continue;
+
+		spc_dbg(chip, "preparing to power off...\n");
+
+		err = chip->power_off_prepare(chip);
+		if (err < 0)
+			spc_warn(chip, "failed to prepare power off: %d\n",
+				 err);
+	}
+
+	list_for_each_entry(chip, &system_power_chips, list) {
+		if (!chip->power_off)
+			continue;
+
+		spc_dbg(chip, "powering off...\n");
+
+		err = chip->power_off(chip);
+		if (err < 0)
+			spc_warn(chip, "failed to power off: %d\n", err);
+	}
+
+	mutex_unlock(&system_power_lock);
+
+	/* XXX for backwards compatibility */
+	if (pm_power_off)
+		pm_power_off();
+
+	return 0;
+}
