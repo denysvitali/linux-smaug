8a1f1cd1cb60f63d6e4c4263ae9d4dd3f04cf8a4
Thierry Reding <treding@nvidia.com>
2017-01-20 15:20:54 +0100

    PM / hibernate: Wire up system-power framework
    Use the system-power framework's equivalent to test for power off
    capability instead of relying on the globally defined pm_power_off()
    function pointer.
    
    The system-power framework implements a fallback that relies on this
    global function in case no system power chips have been registered.
    
    Moving this to the system-power framework allows us to eventually
    remove any traces of pm_power_off() once all handlers have moved over
    to the new framework.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -33,6 +33,7 @@
 #include <linux/genhd.h>
 #include <linux/ktime.h>
 #include <trace/events/power.h>
+#include <linux/system-power.h>
 
 #include "power.h"
 
@@ -632,7 +633,7 @@ static void power_down(void)
 	case HIBERNATION_PLATFORM:
 		hibernation_platform_enter();
 	case HIBERNATION_SHUTDOWN:
-		if (pm_power_off)
+		if (system_can_power_off())
 			kernel_power_off();
 		break;
 	}
