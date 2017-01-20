7a4d7849f85f015981537b11a69fb8fec1d82a83
Thierry Reding <treding@nvidia.com>
2017-01-20 16:42:05 +0100

    arm64: Move to system-power framework
    Use the new system-power framework to reboot and power off the system
    instead of the pm_power_off() and do_kernel_restart() functions.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -33,7 +33,6 @@
 #include <linux/unistd.h>
 #include <linux/user.h>
 #include <linux/delay.h>
-#include <linux/reboot.h>
 #include <linux/interrupt.h>
 #include <linux/init.h>
 #include <linux/cpu.h>
@@ -45,9 +44,9 @@
 #include <linux/random.h>
 #include <linux/hw_breakpoint.h>
 #include <linux/personality.h>
-#include <linux/notifier.h>
 #include <trace/events/power.h>
 #include <linux/percpu.h>
+#include <linux/system-power.h>
 #include <linux/thread_info.h>
 
 #include <asm/alternative.h>
@@ -129,8 +128,7 @@ void machine_power_off(void)
 {
 	local_irq_disable();
 	smp_send_stop();
-	if (pm_power_off)
-		pm_power_off();
+	system_power_off();
 }
 
 /*
@@ -155,8 +153,7 @@ void machine_restart(char *cmd)
 	if (efi_enabled(EFI_RUNTIME_SERVICES))
 		efi_reboot(reboot_mode, NULL);
 
-	/* Now call the architecture specific reboot code. */
-	do_kernel_restart(cmd);
+	system_restart(cmd);
 
 	/*
 	 * Whoops - the architecture was unable to reboot.
