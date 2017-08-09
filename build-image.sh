2b61889910c73be8b726974a35d6c82dd1fc81b2
Denys Vitali <denys.vitali@coelis.ch>
2017-08-10 00:49:10 +0200

    Fix brainfart

--- build-image.sh
+++ build-image.sh
@@ -2,4 +2,5 @@
 lz4c ./arch/arm64/boot/Image Image.lz4
 mkimage -f Image.its Image.fit
 num=$(date +%Y%m%d_%H%M%S)
-mkbootimg --kernel /kernel/linux-smaug/Image.fit --ramdisk /kernel/ramdisk/initramfs.cpio.gz -o /kernel/kitchen/boot-4.13-rc4_$num.img.unsigned
+mkbootimg --kernel /kernel/linux-4.13-rc4/Image.fit --ramdisk /kernel/ramdisk/custom-initramfs.cpio.gz -o /kernel/kitchen/boot-4.13-rc4_$num.img.unsigned
+echo "boot-4.13-rc4_$num.img.unsigned built"
