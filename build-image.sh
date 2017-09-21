eca0100e079d0d0573007f463dfe4fd1a22b959d
Denys Vitali <denys.vitali@coelis.ch>
2017-09-21 22:09:05 +0200

    Switch to 4.14-rc1

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+RELEASE="4.14-rc1"
 lz4c ./arch/arm64/boot/Image Image.lz4
 mkimage -f Image.its Image.fit
 num=$(date +%Y%m%d_%H%M%S)
-mkbootimg --kernel /kernel/linux-4.13-rc4/Image.fit --ramdisk /kernel/ramdisk/custom-initramfs.cpio.gz -o /kernel/kitchen/boot-4.13-rc4_$num.img.unsigned
-echo "boot-4.13-rc4_$num.img.unsigned built"
+mkbootimg --kernel /kernel/linux-smaug/Image.fit --ramdisk /kernel/ramdisk/custom-initramfs.cpio.gz -o /kernel/kitchen/boot-$release_$num.img.unsigned
+echo "boot-$release_$num.img.unsigned built"
