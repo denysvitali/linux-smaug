2876f13ba183858a8f94857e99e89f72d5d9f3a7
Denys Vitali <denys.vitali@coelis.ch>
2017-10-27 21:28:16 +0200

    Make builder dynamic

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-RELEASE="4.14-rc1"
+BUILDTAG="`git symbolic-ref HEAD 2> /dev/null | cut -b 12-`-`git log --pretty=format:\"%h\" -1`"
+RELEASE="$BUILDTAG"
+echo "Building $BUILDTAG"
 lz4c ./arch/arm64/boot/Image Image.lz4
 mkimage -f Image.its Image.fit
 num=$(date +%Y%m%d_%H%M%S)
-mkbootimg --kernel /kernel/linux-smaug/Image.fit --ramdisk /kernel/ramdisk/custom-initramfs.cpio.gz -o /kernel/kitchen/boot-$release_$num.img.unsigned
-echo "boot-$release_$num.img.unsigned built"
+mkbootimg --kernel /kernel/linux-smaug/Image.fit --ramdisk /kernel/ramdisk/custom-initramfs.cpio.gz -o /kernel/kitchen/boot-${RELEASE}_$num.img.unsigned
+echo "boot-${RELEASE}_$num.img.unsigned built"
