1fa8df57d7e1443f0620e4d722f913371889b238
Denys Vitali <denys@denv.it>
2018-04-24 12:56:44 +0200

    Build ramdisk in docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -5,5 +5,7 @@ git clone https://github.com/denysvitali/smaug-custom-initram
 ln -s /kernel/ramdisk/ramdisk.cpio.gz /kernel/ramdisk/custom-initramfs.cpio.gz
 cat > /kernel/ramdisk/build.sh <<- EOF
 #!/bin/bash
-(cd ../smaug-custom-initram/ && find . | cpio -H newc -o | gzip -9 > /kernel/ramdisk/ramdisk.cpio.gz)
+(cd /kernel/smaug-custom-initram/ && find . | cpio -H newc -o | gzip -9 > /kernel/ramdisk/ramdisk.cpio.gz)
 EOF
+chmod u+x /kernel/ramdisk/build.sh
+/kernel/ramdisk/build.sh
